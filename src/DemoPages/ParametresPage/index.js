ade65d70225a3e11ee9c6437d6fe96e3ba9730bb
Ngouda Toure <ngouda1103@gmail.com>
2019-11-05 12:38:02 +0000

    debut mise en place de l'affichage des parametres

--- src/DemoPages/ParametresPage/index.js
+++ src/DemoPages/ParametresPage/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import AppHeader from '../../Layout/AppHeader';
 import AppSidebar from '../../Layout/AppSidebar';
 import AppFooter from '../../Layout/AppFooter';
+import  ShowParametre  from './ShowParametres';
 
 export default class ParametrePage extends Component {
 
@@ -14,7 +15,7 @@ export default class ParametrePage extends Component {
                     <AppSidebar />
                     <div className="app-main__outer">
                         <div className="app-main__inner">
-                           
+                          <ShowParametre/> 
                         </div>
                         <AppFooter />
                     </div>
